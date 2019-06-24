7d7017b4fa56f87acc57fbc25ce4d105c50e636b
Joshua Mummert <jm@joshuamummert.com>
2019-06-24 13:17:45 -0400

    setup webpack for dev svr use

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.bundle.js'
-  }
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './app/index.template.html'})
+    ]
 };
